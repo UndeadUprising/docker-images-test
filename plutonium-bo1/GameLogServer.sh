67442f97d89df82c68f9653e2c109e53994aae01
Classixz <classixz@undeaduprising.net>
2022-06-06 22:40:31 +0200

    Create GameLogServer.sh

---
+++ plutonium-bo1/GameLogServer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Downloading GameLogServer"
+
+if [ ! -d /home/container/GameLogServer ]; then
+	mkdir /home/container/GameLogServer
+fi
+
+wget https://github.com/RaidMax/IW4MAdmin-GameLogServer/releases/download/1.0.0/GameLogServer.zip -O /home/container/GameLogServer/GameLogServer.zip -q --show-progress
+
+cd /home/container/GameLogServer
+unzip -o GameLogServer.zip
+rm GameLogServer.zip
+
+pip install -U --prefix .local -r requirements.txt
