5493b99475b1d18cada75c58bb2e5511a3b70fe7
Classixz <classixz@undeaduprising.net>
2022-06-07 08:35:02 +0200

    Update GameLogServer.sh

--- plutonium-bo1/GameLogServer.sh
+++ plutonium-bo1/GameLogServer.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-echo "Downloading GameLogServer"
-
-python3.9 --version
 
 if [ ! -d /home/container/GameLogServer ]; then
+	echo "Downloading GameLogServer"
 	mkdir /home/container/GameLogServer
+	wget https://github.com/RaidMax/IW4MAdmin-GameLogServer/releases/download/1.0.0/GameLogServer.zip -O /home/container/GameLogServer/GameLogServer.zip -q --show-progress
+
+	cd /home/container/GameLogServer
+	unzip -o GameLogServer.zip
+	rm GameLogServer.zip
+
+	pip install -r requirements.txt
+	pip install python-dotenv
 fi
-
-wget https://github.com/RaidMax/IW4MAdmin-GameLogServer/releases/download/1.0.0/GameLogServer.zip -O /home/container/GameLogServer/GameLogServer.zip -q --show-progress
-
-cd /home/container/GameLogServer
-unzip -o GameLogServer.zip
-rm GameLogServer.zip
-
-export PYTHONPATH="${PYTHONPATH}:/home/container/GameLogServer/.local/bin"
-
-pip install -r requirements.txt
-pip install python-dotenv
-pip install Flask
 
 python3 /home/container/GameLogServer/runserver.py &
