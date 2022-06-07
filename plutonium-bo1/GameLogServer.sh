a0fa142e27eb51da0b006ccb1e72d478feb4dadc
Classixz <classixz@undeaduprising.net>
2022-06-07 08:58:15 +0200

    Update GameLogServer.sh

--- plutonium-bo1/GameLogServer.sh
+++ plutonium-bo1/GameLogServer.sh
@@ -11,6 +11,9 @@ if [ ! -d /home/container/GameLogServer ]; then
 
 	pip install -r requirements.txt
 	pip install python-dotenv
+	
+	echo "Patching runserver.py"
+	wget https://gist.githubusercontent.com/Classixz/49c21a0e8ba54fffae354ad47b879dc8/raw/5fbc3ad654eb89ca2105462fac2d3986f71cea4b/runserver.py -O /home/container/GameLogServer/runserver.py -q --show-progress
 fi
 
 python3 /home/container/GameLogServer/runserver.py &
